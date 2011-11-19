7f5e67cd4a572cc12b541b840efd52859e03463d
salmon-charles <salmon.charles@gmail.com>
2011-11-19 14:42:43 +0000

    test

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-var app = require('express').createServer(), 
-    io = require('socket.io').listen(app),
-    spawn = require('child_process').spawn;
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
 
-var port = 80;
-if (process && process.env && process.env.C9_PORT) {
-    port = process.env.C9_PORT;
-}
-try {
-    app.listen(port);
-} catch(err) {
-    console.error(err);  
+
+var port = process.env.PORT || 8001;
+
+app.listen(port);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200, {'Content-Type': 'text/html', "Content-Length": data.length});
+    res.end(data);
+  });
 }
 
-app.get('/', function (req, res) {
-  res.sendfile('index.html');
-});
+//app.get('/', function (req, res) {
+//  res.sendfile('index.html');
+//});
 
  
 io.sockets.on('connection', function (socket) {
